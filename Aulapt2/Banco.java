5c45234cd029042a6040a96ce13f5e2ab110983c
Caio Mussi <77495559+Caio1102@users.noreply.github.com>
2026-02-26 00:54:59 +0000

    final

--- Aulapt2/Banco.java
+++ Aulapt2/Banco.java
@@ -15,4 +15,25 @@ public class Banco{
         System.out.println("\nContas do Banco" + nome + ":");
         for (Conta c: contas) {System.out.println(c);}
     }
+
+    public void premiarInfinitas(){
+        for (Conta c: contas){
+            if (c instanceof ContaInfinita){
+                c.depositar(1000);
+            }
+        }
+    }
+
+    public void tentarSacar(double v){
+        for (Conta c: contas){c.sacar(v); }
+    }
+
+    public void aumentarLimiteEspeciais(double v){
+        for(Conta c: contas){
+            if (c instanceof ContaEspecial){
+                ContaEspecial e = (ContaEspecial)c;
+                e.aumentar(v);
+            }
+        }
+    }
 }
